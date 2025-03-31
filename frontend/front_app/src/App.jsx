0c57241e0a47d36b5f578bea00f6dd8bb0f396d8
syushen92 <111703023@g.nccu.edu.tw>
2025-03-31 20:51:07 +0800

    update

--- frontend/front_app/src/App.jsx
+++ frontend/front_app/src/App.jsx
@@ -1,6 +1,8 @@
 import {useState, useEffect} from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { Channel } from './Component/Channel/channel';
 import  Login  from './pages/Login';
+import SignUp from './pages/SignUp';
 import './App.module.css';
 
 function App() {
@@ -19,13 +21,24 @@ function App() {
   }, [isLoggedIn])
 
   return (
-    <div className="App">
-      {isLoggedIn ? (
-        <Channel data={initialState} />
-      ) : (
-        <Login onLogin={setIsLoggedIn} />
-      )}
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Redirect to login if not logged in */}
+          <Route path="/" element={isLoggedIn ? <Channel data={initialState}/> : <Navigate to="/login" />} />        
+          
+          {/* Signup page */}
+          <Route>
+            <Route path="/signup" element={<SignUp />} />
+          </Route>
+          
+          {/* Login page */}
+          <Route>
+            <Route path="/login" element={<Login onLogin={setIsLoggedIn}/>} />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
